ea7de60fb082117ddc4939285ad942789bdf8574
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-07-09 20:52:01 +0530

    Google OAuth using @react-oauth/google as react-google-login deprecated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import vsocial from "../utils/vsocial.png";
 
 const Navbar = () => {
+	const user = false;
 	return (
 		<div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
 			<Link href='/'>
@@ -16,6 +18,19 @@ const Navbar = () => {
 					/>
 				</div>
 			</Link>
+
+			<div>SEARCH</div>
+
+			<div>
+				{user ? (
+					<div>Logged In</div>
+				) : (
+					<GoogleLogin
+						onSuccess={(res) => console.log(res)}
+						onError={() => console.log("Error")}
+					/>
+				)}
+			</div>
 		</div>
 	);
 };
