df97917dc5829c7da602fa3bbab221991abbbff8
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-07-12 19:00:02 +0530

    Showing liked videos and videos of a user on profile pg

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,5 +1,9 @@
 import axios from "axios";
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { GoVerified } from "react-icons/go";
+import NoResults from "../../components/NoResults";
+import VideoCard from "../../components/VideoCard";
 import { IUser, Video } from "../../types";
 import { BASE_URL } from "../../utils";
 
@@ -12,7 +16,77 @@ interface IProps {
 }
 
 const Profile = ({ data }: IProps) => {
-	return <div>Profile</div>;
+	const { user, userVideos, userLikedVideos } = data;
+	const [showUserVideos, setShowUserVideos] = useState(true);
+	const [videosList, setVideosList] = useState<Video[]>([]);
+
+	const videos = showUserVideos ? "border-b-2 border-black" : "text-gray-400";
+	const liked = !showUserVideos ? "border-b-2 border-black" : "text-gray-400";
+
+	useEffect(() => {
+		if (showUserVideos) {
+			setVideosList(userVideos);
+		} else {
+			setVideosList(userLikedVideos);
+		}
+	}, [showUserVideos, userLikedVideos, userVideos]);
+
+	return (
+		<div className='w-full'>
+			<div className='flex gap-6 md:gap-10 mb-4 bg-white w-full'>
+				<div className='w-16 h-16 md:w-32 md:h-32'>
+					<Image
+						src={user.image}
+						width={120}
+						height={120}
+						className='rounded-full'
+						alt='user profile'
+						layout='responsive'
+					/>
+				</div>
+
+				<div className='flex flex-col justify-center'>
+					<p className='justify-center md:text-2xl tracking-wider flex gap-1 items-center text-md font-bold text-primary lowercase'>
+						{user.userName.replaceAll(" ", "")}{" "}
+						<GoVerified className='text-blue-400' />{" "}
+					</p>
+					<p className='text-gray-400 capitalize text-xs md:text-xl'>
+						{user.userName}
+					</p>
+				</div>
+			</div>
+
+			<div>
+				<div className='flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full'>
+					<p
+						className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`}
+						onClick={() => setShowUserVideos(true)}
+					>
+						Videos
+					</p>
+
+					<p
+						className={`text-xl font-semibold cursor-pointer mt-2 ${liked}`}
+						onClick={() => setShowUserVideos(false)}
+					>
+						Liked
+					</p>
+				</div>
+
+				<div className='flex gap-6 flex-wrap md:justify-start'>
+					{videosList.length > 0 ? (
+						videosList.map((post: Video, idx: number) => (
+							<VideoCard post={post} key={idx} />
+						))
+					) : (
+						<NoResults
+							text={`No ${showUserVideos ? "" : "Liked"} Videos Yet`}
+						/>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export const getServerSideProps = async ({
