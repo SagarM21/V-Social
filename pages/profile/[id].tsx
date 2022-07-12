5cef8ff81e056c26cddfaf96aa523a95924b674e
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-07-12 18:32:01 +0530

    Server side props for profil pg

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,7 +1,32 @@
+import axios from "axios";
 import React from "react";
+import { IUser, Video } from "../../types";
+import { BASE_URL } from "../../utils";
 
-const Profile = () => {
+interface IProps {
+	data: {
+		user: IUser;
+		userVideos: Video[];
+		userLikedVideos: Video[];
+	};
+}
+
+const Profile = ({ data }: IProps) => {
 	return <div>Profile</div>;
+};
+
+export const getServerSideProps = async ({
+	params: { id }, // yha pr vhi name aata h jo file ka name h
+}: {
+	params: { id: string };
+}) => {
+	const res = await axios.get(`${BASE_URL}/profile/${id}`);
+
+	return {
+		props: {
+			data: res.data,
+		},
+	};
 };
 
 export default Profile;
