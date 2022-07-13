a212eec3514d3cc02af87c2b65aff17a1e6b10df
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-07-13 20:54:17 +0530

    search accounts completed

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -1,18 +1,27 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
-import { Video } from "../../types";
+import { IUser, Video } from "../../types";
 import { BASE_URL } from "../../utils";
 import NoResults from "../../components/NoResults";
 import VideoCard from "../../components/VideoCard";
+import useAuthStore from "../../store/authStore";
+import Link from "next/link";
+import Image from "next/image";
+import { GoVerified } from "react-icons/go";
 
 const Search = ({ videos }: { videos: Video[] }) => {
 	const [isAccounts, setIsAccounts] = useState(false);
 	const router = useRouter();
-	const { searchTerm } = router.query;
+	const { searchTerm }: any = router.query;
+	const { allUsers } = useAuthStore();
 
 	const accounts = isAccounts ? "border-b-2 border-black" : "text-gray-400";
 	const isVideos = !isAccounts ? "border-b-2 border-black" : "text-gray-400";
+
+	const searchedAccounts = allUsers.filter((user: IUser) =>
+		user.userName.toLowerCase().includes(searchTerm.toLowerCase())
+	);
 
 	return (
 		<div className='w-full'>
@@ -32,7 +41,37 @@ const Search = ({ videos }: { videos: Video[] }) => {
 				</p>
 			</div>
 			{isAccounts ? (
-				<div>Accounts</div>
+				<div className=''>
+					{searchedAccounts.length > 0 ? (
+						searchedAccounts.map((user: IUser, idx: number) => (
+							<Link href={`/profile/${user._id}`} key={idx}>
+								<div className='flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200'>
+									<div>
+										<Image
+											src={user.image}
+											width={50}
+											height={50}
+											className='rounded-full'
+											alt='user profile'
+										/>
+									</div>
+
+									<div className='hidden xl:block'>
+										<p className='flex gap-1 items-center text-md font-bold text-primary lowercase'>
+											{user.userName.replaceAll(" ", "")}{" "}
+											<GoVerified className='text-blue-400' />{" "}
+										</p>
+										<p className='text-gray-400 capitalize text-xs'>
+											{user.userName}
+										</p>
+									</div>
+								</div>
+							</Link>
+						))
+					) : (
+						<NoResults text={`No video results for ${searchTerm}`} />
+					)}
+				</div>
 			) : (
 				<div>
 					{videos.length ? (
